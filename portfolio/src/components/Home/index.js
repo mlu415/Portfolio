556155c848dde4ad5782a99afdf52da74bb10774
michelle <mlu415@aucklanduni.ac.nz>
2023-03-06 11:08:01 +1300

    add styling to home page

--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -1,7 +1,26 @@
 import "./index.scss";
+import LogoS from "../../assets/images/logo-s.png";
+import LogoSubtitle from "../../assets/images/logo_sub.png";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <></>;
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm
+          <img src={LogoS} alt="name" />
+          ichelle
+          <br />
+          Web Developer
+        </h1>
+        <h2>Software Engineering Student</h2>
+        <Link to="/contact" className="flat-button">
+          Contact Me
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
