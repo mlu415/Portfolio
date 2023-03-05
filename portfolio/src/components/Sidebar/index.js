4d2a1048ddaa9be228d6b0ee8192eb828f060143
michelle <mlu415@aucklanduni.ac.nz>
2023-03-06 00:27:53 +1300

    update sidebar

--- portfolio/src/components/Sidebar/index.js
+++ portfolio/src/components/Sidebar/index.js
@@ -1,5 +1,22 @@
 import "./index.scss";
+import LogoS from "../../assets/images/logo-s.png";
+import LogoSubtitle from "../../assets/images/logo_sub.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { Link, NavLink } from "react-router-dom";
 
-const Sidebar = () => <>Hello from sidebar</>;
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoS} alt="Logo" />
+      <img className="sub-logo" src={LogoSubtitle} alt="tests" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+  </div>
+);
 
 export default Sidebar;
