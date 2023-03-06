a6177ef69d086847cfb458f9ff9f9901e50b87c2
michelle <mlu415@aucklanduni.ac.nz>
2023-03-06 21:33:03 +1300

    bug fix projects

--- portfolio/src/components/Project/index.js
+++ portfolio/src/components/Project/index.js
@@ -1,18 +1,74 @@
-import "./index.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPuzzlePiece } from "@fortawesome/free-solid-svg-icons";
-
-import { Link } from "react-router-dom";
+import "./index.css";
+import dataProject from "../datasheets/dataProject";
+import { useState } from "react";
 
 const Project = () => {
-  return (
-    <div className="container project-page">
-      <div className="text-zone">
-        <h1>Projects</h1>
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-        <p>LETS HOPE THIS WORKS</p>
+  const goToProject = (projectIndex) => {
+    setCurrentIndex(projectIndex);
+  };
+  return (
+    <div className="project-parent">
+      <div className="container project-page">
+        <div className="text-zone">
+          <h1>Projects</h1>
+          <img
+            src={dataProject[currentIndex].image}
+            alt={`${dataProject[currentIndex].title}`}
+          ></img>
+        </div>
+      </div>
+      <div className="project-right">
+        {/* <div className="projects"> */}
+        <div className="brackets-wrapper">
+          <div className="projects-info">
+            <h1 className="project-title">{dataProject[currentIndex].title}</h1>
+            <div className="project-links">
+              <a
+                href={dataProject[currentIndex].demo}
+                target="_blank"
+                rel="noreferrer"
+              >
+                Demo
+              </a>
+              <a
+                href={dataProject[currentIndex].sourceCode}
+                target="_blank"
+                rel="noreferrer"
+              >
+                Source Code
+              </a>
+            </div>
+            <ul className="project-tech">
+              {dataProject[currentIndex].tech.map((tech, techIndex) => (
+                <li key={techIndex}>{tech}</li>
+              ))}
+            </ul>
+            <p className="project-description">
+              {dataProject[currentIndex].description}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="projects-nav">
+        {dataProject.map((project, projectIndex) => (
+          <div
+            style={{
+              background: `${
+                projectIndex === currentIndex
+                  ? "var(--bg-primary)"
+                  : "var(--bg-secondary)"
+              }`,
+            }}
+            key={projectIndex}
+            onClick={() => goToProject(projectIndex)}
+          ></div>
+        ))}
       </div>
     </div>
+    // </div>
+    // </div>
   );
 };
 
---
+++ portfolio/src/components/datasheets/dataProject.js
@@ -0,0 +1,24 @@
+import unihackImg from "../../assets/images/Unihack.png";
+
+const projectData = [
+  {
+    title: "UNIHACK",
+    tech: ["JavaScript", "React", "Next.js", "Postgres", "Docker"],
+    description: "Team Management App for over 400 participants",
+    image: unihackImg,
+    sourceCode: "Confidential",
+    demo: "https://manage.unihack.net/",
+  },
+  {
+    title: "Gamification of Behavioural Experiments",
+    tech: ["MongoDb", "Express", "Node.js", "TypeScript", "React"],
+    description:
+      "This project was created for the University of Auckland Psychology department to evaluate human learning.",
+    image: "",
+    sourceCode:
+      "https://github.com/mlu415/Gamification_Behavioural_Experiments_Reupload",
+    demo: "",
+  },
+];
+
+export default projectData;
