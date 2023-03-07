c0eda0aa2bb9e62ef9cce92682fdd302fe67f176
michelle <mlu415@aucklanduni.ac.nz>
2023-03-08 09:44:09 +1300

    before trying different approach

--- portfolio/src/components/Project/index.js
+++ portfolio/src/components/Project/index.js
@@ -20,51 +20,54 @@ const Project = () => {
         </div>
       </div>
       <div className="project-right">
-        {/* <div className="projects"> */}
-        <div className="brackets-wrapper">
-          <div className="projects-info">
-            <h1 className="project-title">{dataProject[currentIndex].title}</h1>
-            <div className="project-links">
-              <a
-                href={dataProject[currentIndex].demo}
-                target="_blank"
-                rel="noreferrer"
-              >
-                Demo
-              </a>
-              <a
-                href={dataProject[currentIndex].sourceCode}
-                target="_blank"
-                rel="noreferrer"
-              >
-                Source Code
-              </a>
+        <div className="projects">
+          <div className="brackets-wrapper">
+            <div className="projects-info">
+              <h1 className="project-title">
+                {dataProject[currentIndex].title}
+              </h1>
+              <div className="project-links">
+                <a
+                  href={dataProject[currentIndex].demo}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Demo
+                </a>
+                <a
+                  href={dataProject[currentIndex].sourceCode}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Source Code
+                </a>
+              </div>
+              <ul className="project-tech">
+                {dataProject[currentIndex].tech.map((tech, techIndex) => (
+                  <li key={techIndex}>{tech}</li>
+                ))}
+              </ul>
+              <p className="project-description">
+                {dataProject[currentIndex].description}
+              </p>
             </div>
-            <ul className="project-tech">
-              {dataProject[currentIndex].tech.map((tech, techIndex) => (
-                <li key={techIndex}>{tech}</li>
-              ))}
-            </ul>
-            <p className="project-description">
-              {dataProject[currentIndex].description}
-            </p>
           </div>
         </div>
-      </div>
-      <div className="projects-nav">
-        {dataProject.map((project, projectIndex) => (
-          <div
-            style={{
-              background: `${
-                projectIndex === currentIndex
-                  ? "var(--bg-primary)"
-                  : "var(--bg-secondary)"
-              }`,
-            }}
-            key={projectIndex}
-            onClick={() => goToProject(projectIndex)}
-          ></div>
-        ))}
+        <div className="projects-nav">
+          {dataProject.map((project, projectIndex) => (
+            <div
+              style={{
+                background: `${
+                  projectIndex === currentIndex
+                    ? "var(--bg-primary)"
+                    : "var(--bg-secondary)"
+                }`,
+              }}
+              key={projectIndex}
+              onClick={() => goToProject(projectIndex)}
+            ></div>
+          ))}
+        </div>
       </div>
     </div>
     // </div>
